d4f6fb49511bfe5d712405dd4affc590c5e49686
Nafiul <nafiulhaque22@gmail.com>
2021-05-29 09:26:54 +0600

    component tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MainComponent from './components/Maincomponent'
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      
+      <MainComponent/>
     </div>
   );
 }
