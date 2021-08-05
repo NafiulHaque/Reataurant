91dc8da573faf23d6b53141a3749cc483178f4f8
Nafiul <nafiulhaque22@gmail.com>
2021-08-05 21:14:58 +0600

    menu design

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,26 +1,31 @@
-import {Card,CardBody,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 const MenuItem = props => {
-    
-    return(
+
+    return (
         <div>
-            <Card style={{ margin: "10px"} }>
+            <Card style={{ margin: "10px" }}>
                 <CardBody>
-                    <CardImg 
-                    width="100%" 
-                    alt={props.dish.name} 
-                    src={props.dish.image}
-                    style={{opacity:"0.5"}}
+                    <CardImg
+                        width="100%"
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                        style={{ opacity: "0.7", }}
                     />
 
                     <CardImgOverlay>
-                        <CardTitle 
-                        style={{ cursor: "pointer"}}
-                        onClick= {props.DishSelect}
+                        <CardTitle
+                            style={{ cursor: "pointer", color: "white", fontSize: "25px" }}
+                            onClick={props.DishSelect}
                         >{props.dish.name}
-                        
+
                         </CardTitle>
+
                     </CardImgOverlay>
+
+                    {props.dish.description}
+
+                    <h4>Price: {props.dish.price} Tk</h4>
                 </CardBody>
             </Card>
         </div>
